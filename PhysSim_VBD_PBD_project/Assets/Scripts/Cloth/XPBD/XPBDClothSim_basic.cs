3b75fcbacefd474cc17310e42102485e386b6de1
Márton Biró <biromarci04@gmail.com>
2026-04-27 15:23:36 +0200

    Stripped down XPBD fully done

--- PhysSim_VBD_PBD_project/Assets/Scripts/Cloth/XPBD/XPBDClothSim_basic.cs
+++ PhysSim_VBD_PBD_project/Assets/Scripts/Cloth/XPBD/XPBDClothSim_basic.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
 
 namespace XPBD_stripped_down_version
@@ -12,6 +11,12 @@ namespace XPBD_stripped_down_version
         public float stretchingCompliance = 0.0f;
         public float shearCompliance = 0.0001f;
         public float bendingCompliance = 0.01f;
+
+        public bool logMsPerFrame = true;
+        private const int logEveryNFrames = 10;
+
+        private string performanceText = "XPBD Simulation: -- ms/frame";
+        private GUIStyle performanceStyle;
 
         private Vector3 gravity = new Vector3(0, -9.81f, 0);
 
@@ -43,27 +48,15 @@ namespace XPBD_stripped_down_version
             tr = transform;
             mesh.MarkDynamic();
 
-            MeshCollider meshCollider = gameObject.GetComponent<MeshCollider>();
-            if (meshCollider != null) Destroy(meshCollider);
+            Destroy(gameObject.GetComponent<MeshCollider>());
 
             Vector3[] localVerts = mesh.vertices;
             numVerts = localVerts.Length;
             renderVertices = new Vector3[numVerts];
 
-            // Get all different x and y values for numX and numY
             float eps = 0.001f;
-            var xCoords = new List<float>();
-            var yCoords = new List<float>();
-
-            foreach (var v in localVerts)
-            {
-                if (!xCoords.Any(x => Mathf.Abs(x - v.x) < eps)) xCoords.Add(v.x);
-                if (!yCoords.Any(y => Mathf.Abs(y - v.y) < eps)) yCoords.Add(v.y);
-            }
-
-            xCoords.Sort();
-            yCoords.Sort();
-
+            List<float> xCoords = GetDistinctSortedCoordinates(localVerts, v => v.x, eps);
+            List<float> yCoords = GetDistinctSortedCoordinates(localVerts, v => v.y, eps);
             int numX = xCoords.Count;
             int numY = yCoords.Count;
 
@@ -73,35 +66,18 @@ namespace XPBD_stripped_down_version
             invMasses = new float[numVerts];
             Array.Fill(invMasses, 1.0f);
 
-            for (int iy = 0; iy < numY; iy++)
-                for (int ix = 0; ix < numX; ix++)
-                {
-                    int idx = iy * numX + ix;
-                    Vector3 localPos = new Vector3(xCoords[ix], yCoords[iy], 0f);
-                    positions[idx] = tr.TransformPoint(localPos);
-                }
-
-            int topLeft = (numY - 1) * numX;
-            int topRight = (numY - 1) * numX + (numX - 1);
-
-            invMasses[topLeft] = 0f;
-            invMasses[topRight] = 0f;
-
-            // Map each mesh vertex to its grid particle index
-            meshToGrid = new int[numVerts];
-            for (int vi = 0; vi < numVerts; vi++)
-            {
-                int ix = xCoords.FindIndex(x => Mathf.Abs(x - localVerts[vi].x) < eps);
-                int iy = yCoords.FindIndex(y => Mathf.Abs(y - localVerts[vi].y) < eps);
-                meshToGrid[vi] = iy * numX + ix;
-            }
-
+            BuildSimulationGrid(xCoords, yCoords, numX, numY);
+            SetFixedVertices(numX, numY);
+            BuildMeshToGrid(localVerts, xCoords, yCoords, eps, numX);
             InitConstraints(numX, numY);
         }
 
-        void FixedUpdate()
+        void Update()
         {
-            float sdt = Time.fixedDeltaTime / numSubsteps;
+            bool shouldLogPerformance = logMsPerFrame && Time.frameCount % logEveryNFrames == 0;
+            double simStartTime = shouldLogPerformance ? Time.realtimeSinceStartupAsDouble : 0;
+
+            float sdt = Time.deltaTime / numSubsteps;
             float invSdt2 = 1.0f / (sdt * sdt);
 
             for (int step = 0; step < numSubsteps; step++)
@@ -115,8 +91,9 @@ namespace XPBD_stripped_down_version
                 }
 
                 // Solve constraints
-                foreach (var constraint in constraints)
+                for (int i = 0; i < constraints.Length; i++)
                 {
+                    var constraint = constraints[i];
                     int id0 = constraint.p1Idx;
                     int id1 = constraint.p2Idx;
                     float w0 = invMasses[id0];
@@ -144,64 +121,119 @@ namespace XPBD_stripped_down_version
                 }
             }
 
+            Matrix4x4 worldToLocal = tr.worldToLocalMatrix;
             for (int i = 0; i < numVerts; i++)
-                renderVertices[i] = tr.InverseTransformPoint(positions[meshToGrid[i]]);
+                renderVertices[i] = worldToLocal.MultiplyPoint3x4(positions[meshToGrid[i]]);
 
-            mesh.SetVertices(renderVertices);
+            mesh.SetVertices(renderVertices, 0, numVerts, UnityEngine.Rendering.MeshUpdateFlags.DontRecalculateBounds);
             mesh.RecalculateNormals();
+
+            if (shouldLogPerformance)
+            {
+                double simEndTime = Time.realtimeSinceStartupAsDouble;
+                double msPerFrame = (simEndTime - simStartTime) * 1000.0;
+                performanceText = $"XPBD Simulation: {msPerFrame:F2} ms/frame";
+            }
+        }
+
+        void OnGUI()
+        {
+            if (!logMsPerFrame) return;
+
+            if (performanceStyle == null)
+            {
+                performanceStyle = new GUIStyle(GUI.skin.label);
+                performanceStyle.fontSize = 14;
+                performanceStyle.normal.textColor = Color.white;
+            }
+
+            GUI.Label(new Rect(10, 10, 420, 30), performanceText, performanceStyle);
+        }
+
+        private static List<float> GetDistinctSortedCoordinates(Vector3[] vertices, Func<Vector3, float> selector, float epsilon)
+        {
+            var coords = new List<float>();
+
+            foreach (Vector3 v in vertices)
+            {
+                float value = selector(v);
+                if (!coords.Any(c => Mathf.Abs(c - value) < epsilon))
+                    coords.Add(value);
+            }
+
+            coords.Sort();
+            return coords;
+        }
+
+        private void SetFixedVertices(int numX, int numY)
+        {
+            int topLeft = (numY - 1) * numX;
+            int topRight = (numY - 1) * numX + (numX - 1);
+
+            invMasses[topLeft] = 0f;
+            invMasses[topRight] = 0f;
+        }
+
+        private void BuildSimulationGrid(List<float> xCoords, List<float> yCoords, int numX, int numY)
+        {
+            for (int iy = 0; iy < numY; iy++)
+                for (int ix = 0; ix < numX; ix++)
+                {
+                    int idx = iy * numX + ix;
+                    Vector3 localPos = new Vector3(xCoords[ix], yCoords[iy], 0f);
+                    positions[idx] = tr.TransformPoint(localPos);
+                }
+        }
+
+        private void BuildMeshToGrid(Vector3[] localVerts, List<float> xCoords, List<float> yCoords, float eps, int numX)
+        {
+            meshToGrid = new int[numVerts];
+            for (int i = 0; i < numVerts; i++)
+            {
+                int ix = xCoords.FindIndex(x => Mathf.Abs(x - localVerts[i].x) < eps);
+                int iy = yCoords.FindIndex(y => Mathf.Abs(y - localVerts[i].y) < eps);
+                meshToGrid[i] = iy * numX + ix;
+            }
         }
 
         private void InitConstraints(int numX, int numY)
         {
-            var offsets = new int[]
-            {
-                0, 0,  1, 0,  // stretch horizontal
-                0, 0,  0, 1,  // stretch vertical
-                0, 0,  1, 1,  // shear
-                1, 0,  0, 1,  // shear (other)
-                0, 0,  2, 0,  // bend horizontal
-                0, 0,  0, 2,  // bend vertical
-            };
-            var compliances = new float[]
-            {
-                stretchingCompliance, stretchingCompliance,
-                shearCompliance,      shearCompliance,
-                bendingCompliance,    bendingCompliance
-            };
-
             var constraintsList = new List<DistanceConstraint>();
 
-            for (int type = 0; type < 6; type++)
-            {
-                int p = type * 4;
-                int offset_i0 = offsets[p];
-                int offset_j0 = offsets[p + 1];
-                int offset_i1 = offsets[p + 2];
-                int offset_j1 = offsets[p + 3];
-
-                for (int iy = 0; iy < numY; iy++)
-                    for (int ix = 0; ix < numX; ix++)
-                    {
-                        int i0 = ix + offset_i0;
-                        int j0 = iy + offset_j0;
-                        int i1 = ix + offset_i1;
-                        int j1 = iy + offset_j1;
-                        if (i0 < numX && j0 < numY && i1 < numX && j1 < numY)
-                        {
-                            int p1 = j0 * numX + i0;
-                            int p2 = j1 * numX + i1;
-                            constraintsList.Add(new DistanceConstraint
-                            {
-                                p1Idx = p1,
-                                p2Idx = p2,
-                                restLength = Vector3.Distance(positions[p1], positions[p2]),
-                                compliance = compliances[type]
-                            });
-                        }
-                    }
-            }
+            AddConstraint(constraintsList, 0, 0, 1, 0, stretchingCompliance, numX, numY);     // stretch horizontal
+            AddConstraint(constraintsList, 0, 0, 0, 1, stretchingCompliance, numX, numY);     // stretch vertical
+            AddConstraint(constraintsList, 0, 0, 1, 1, shearCompliance, numX, numY);          // shear down
+            AddConstraint(constraintsList, 1, 0, 0, 1, shearCompliance, numX, numY);          // shear up
+            AddConstraint(constraintsList, 0, 0, 2, 0, bendingCompliance, numX, numY);        // bend horizontal
+            AddConstraint(constraintsList, 0, 0, 0, 2, bendingCompliance, numX, numY);        // bend vertical
 
             constraints = constraintsList.ToArray();
+        }
+
+        private void AddConstraint(List<DistanceConstraint> constraintsList, int offset_i0, int offset_j0, int offset_i1, int offset_j1, float compliance, int numX, int numY)
+        {
+            for (int iy = 0; iy < numY; iy++)
+                for (int ix = 0; ix < numX; ix++)
+                {
+                    int i0 = ix + offset_i0;
+                    int j0 = iy + offset_j0;
+                    int i1 = ix + offset_i1;
+                    int j1 = iy + offset_j1;
+
+                    if (i0 < numX && j0 < numY && i1 < numX && j1 < numY)
+                    {
+                        int p1 = j0 * numX + i0;
+                        int p2 = j1 * numX + i1;
+
+                        constraintsList.Add(new DistanceConstraint
+                        {
+                            p1Idx = p1,
+                            p2Idx = p2,
+                            restLength = Vector3.Distance(positions[p1], positions[p2]),
+                            compliance = compliance
+                        });
+                    }
+                }
         }
     }
 }
